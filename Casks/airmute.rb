e801dd99a0e76c7a5a32e2f7d9f007fc247690b9
William M <cominatyou@cominatyou.com>
2025-08-21 00:03:09 -0500

    Initial commit

---
+++ Casks/airmute.rb
@@ -0,0 +1,14 @@
+cask "airmute" do
+    version "2.2.2"
+    sha256 "25b70f47f86c0a3e5c2ebf9968bee0498914c90a15580e3d7deb755e78196366"
+    url "https://github.com/Solarphlare/AirMute/releases/download/v#{version}/AirMute.dmg"
+    name "AirMute"
+    desc "Mute your mic in Discord on Mac with your AirPods."
+    homepage "https://github.com/Solarphlare/AirMute"
+    app "AirMute.app"
+	
+    livecheck do
+        url :homepage
+        strategy :github_latest
+    end
+end
